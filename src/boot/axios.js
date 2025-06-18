ec06b83d789352b3f66ad308a67142a913106e03
tmeftah <sudtechnics@gmail.com>
2025-06-18 14:58:57 +0200

    make the api url as env variable

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'http://192.168.178.78:5008' })
+const api = axios.create({ baseURL: process.env.API_BASE_URL })
 
 export default defineBoot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
