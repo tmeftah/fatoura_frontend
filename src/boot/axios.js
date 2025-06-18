e2d44dae31a36485c840c291c6b9b951e7d691e8
tmeftah <sudtechnics@gmail.com>
2025-06-18 19:45:00 +0200

    remove log

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -8,7 +8,6 @@ import axios from 'axios'
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({ baseURL: process.env.API_URL })
-console.log(process.env.API_URL)
 
 export default defineBoot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
